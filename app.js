585fce8c35391f5b8338f93ed8ccb6fc0bec289c
vansung <luisgerardoaguilarpadilla@gmail.com>
2021-01-03 12:56:49 -0700

    Performance fixed.

--- app.js
+++ app.js
@@ -1,87 +1,45 @@
 const auth = "563492ad6f91700001000001b75b3f9ab4664bd7b5b752c4f00a8179 "
 const animal = document.querySelector('.animal');
+let flag = false;
 
-async function setContent(){
-
-    for(let i = 0; i < pictures.length;i++ ){
-        console.log(pictures[i])
+async function getContent(type, value) {
 
     const promise = await fetch(
-        `https://api.pexels.com/v1/search?query=${pictures[i]}&per_page=80`,
+        `https://api.pexels.com/v1/search?query=${type}&per_page=${1}&page=${value}`,
         {
             method: "GET",
             headers: {
                 Accept: "application/json",
                 Authorization: auth,
-                
+
             },
         }
     );
 
-    console.log(promise.headers.values)
     const object = await promise.json();
-
-
-     arrays.push(object.photos);
-
-    }
+    animal.src = object.photos[0].src.medium;
 }
 
-let flag = false;
-    function buttonClick(value){
-
-        if(!flag){
-            animal.setAttribute("border","2px solid");
-                flag = true;
-            }
-    
-        const randomNum = parseInt(getRandomArbitrary(1 ,80));
-
-        if(value === 'DOG'){
-            console.log('entre a perros')
-
-
-          animal.src = arrays[0][randomNum].src.medium;
-          return;  
-        }
-
-        
-        if(value === 'CAT'){
-            console.log('entre a gatos')
-            animal.src = arrays[1][randomNum].src.medium;
-            return;  
-          }
-
-        if(value === 'BEAR'){
-            console.log('entre a osso')
-            animal.src = arrays[2][randomNum].src.medium;
-            return;  
-          }
-
-        if(value === 'FISH'){
-            console.log('entre a peces')
-
-            animal.src = arrays[3][randomNum].src.medium;
-            return;  
-          }
-
-        if(value === 'BIRD'){
-            console.log('entre a pajaritos')
-            animal.src = arrays[4][randomNum].src.medium;
-            return;  
-          }
+function buttonClick(type) {
+    if (!flag) {
+        animal.setAttribute("border", "2px solid");
+        flag = true;
     }
-    function init(){
-        setContent();
-        document.querySelector(".header-buttons").addEventListener("click",function(event){
-            buttonClick(event.target.innerText);
-        });
-    
-    }
-    
-    
-    function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+    const randomNum = parseInt(getRandomNumber(1, 80));
+    animal.src = getContent(type, randomNum)
+
 }
-    
-    init();
+
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function init() {
+    document.querySelector(".header-buttons").addEventListener("click", function (event) {
+        buttonClick(event.target.innerText);
+    });
+
+}
+
+
+init();
